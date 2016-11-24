32dadab618f1f87c5f6dbf6869937c2c73efd57f
Yuriy <osoftteam@yahoo.com>
2016-11-24 14:19:12 -0500

    tabs -> spaces

--- autotest/main.cpp
+++ autotest/main.cpp
@@ -8,37 +8,37 @@ using namespace googleQt;
 
 int main(int argc, char *argv[]) {
 
-	QCoreApplication app(argc, argv);
+    QCoreApplication app(argc, argv);
 
-	if (argc != 2) {
-		std::string  s = "\nUsage: COMMAND <destination-file>\n"
-		"\n"
-		"<destination-file>: The file where results of autotest procedure will be generated"
-		" Press ENTER to proceed.";
+    if (argc != 2) {
+        std::string  s = "\nUsage: COMMAND <destination-file>\n"
+        "\n"
+        "<destination-file>: The file where results of autotest procedure will be generated"
+        " Press ENTER to proceed.";
 
-		std::cout << s << std::endl;
-		std::cin.ignore();
-		return 0;
-	}
+        std::cout << s << std::endl;
+        std::cin.ignore();
+        return 0;
+    }
 
-	QString argResFile = argv[1];
-	std::unique_ptr<ApiAppInfo>appInfo(new ApiAppInfo);
-	appInfo->setKeySecret("my-key", "my-secret");
-	std::unique_ptr<ApiAuthInfo> authInfo(new ApiAuthInfo());
-	GoogleClient c(appInfo.release(), authInfo.release(), "me@gmail.com");
-	{
-		GoogleAutotest autotest(c);
-		if (!autotest.init(argResFile)) {
-			std::cout << "Error opening destination file " << argResFile.toStdString() << std::endl;
-			std::cin.ignore();
-			return 0;
-		}
-		std::cout << "Press ENTER to start autotest. The result will be generated: " << argResFile.toStdString() << std::endl;
-		std::cin.ignore();
-		autotest.generateCalls();
-		std::cout << "finished" << std::endl;
-	}
-	std::cin.ignore();
+    QString argResFile = argv[1];
+    std::unique_ptr<ApiAppInfo>appInfo(new ApiAppInfo);
+    appInfo->setKeySecret("my-key", "my-secret");
+    std::unique_ptr<ApiAuthInfo> authInfo(new ApiAuthInfo());
+    GoogleClient c(appInfo.release(), authInfo.release(), "me@gmail.com");
+    {
+        GoogleAutotest autotest(c);
+        if (!autotest.init(argResFile)) {
+            std::cout << "Error opening destination file " << argResFile.toStdString() << std::endl;
+            std::cin.ignore();
+            return 0;
+        }
+        std::cout << "Press ENTER to start autotest. The result will be generated: " << argResFile.toStdString() << std::endl;
+        std::cin.ignore();
+        autotest.generateCalls();
+        std::cout << "finished" << std::endl;
+    }
+    std::cin.ignore();
 
     return 0;
 }
