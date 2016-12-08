4e5cf2b03556d500a2e61f6a0a5bc55447473e4e
Yuriy <osoftteam@yahoo.com>
2016-12-08 07:00:40 -0500

    added new async task

---
+++ src/google/endpoint/GoogleTask.cpp
@@ -0,0 +1,49 @@
+#include "GoogleTask.h"
+#include "ApiEndpoint.h"
+
+using namespace googleQt;
+
+///GoogleBaseTask
+bool GoogleBaseTask::waitForResult()const
+{
+	if (!isCompleted() && !isFailed())
+	{
+		m_in_wait_loop = true;
+		m_endpoint.runEventsLoop();
+	}
+
+	return isCompleted();
+};
+
+void GoogleBaseTask::notifyOnFinished()
+{
+	emit finished();
+	if (m_in_wait_loop)
+	{
+		m_endpoint.exitEventsLoop();
+	}
+};
+
+void GoogleBaseTask::waitUntillFinishedOrCancelled()
+{
+	m_endpoint.runEventsLoop();
+}
+
+void GoogleVoidTask::waitForResultAndRelease()
+{
+	if (!isCompleted() && !isFailed())
+	{
+		m_in_wait_loop = true;
+		m_endpoint.runEventsLoop();
+	}
+
+	if (isFailed())
+	{
+		std::unique_ptr<GoogleException> ex;
+		ex = std::move(m_failed);
+		deleteLater();
+		if (ex)
+			ex->raise();
+	}
+	deleteLater();
+};
