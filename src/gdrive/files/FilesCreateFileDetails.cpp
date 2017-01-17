5387ed217904cddfee4915437e6d7a10063613c1
Yuriy <osoftteam@yahoo.com>
2017-01-17 06:45:38 -0500

    added argument class for file create

---
+++ src/gdrive/files/FilesCreateFileDetails.cpp
@@ -0,0 +1,98 @@
+/**********************************************************
+ DO NOT EDIT
+ This file was generated from stone specification "files"
+ www.prokarpaty.net
+***********************************************************/
+
+#include "gdrive/files/FilesCreateFileDetails.h"
+using namespace googleQt;
+
+namespace googleQt{
+
+namespace files{
+///CreateFileDetails
+
+CreateFileDetails::operator QJsonObject()const{
+    QJsonObject js;
+    this->toJson(js);
+    return js;
+}
+
+
+void CreateFileDetails::toJson(QJsonObject& js)const{
+
+    if(!m_id.isEmpty())
+        js["id"] = QString(m_id);
+    if(!m_mimeType.isEmpty())
+        js["mimeType"] = QString(m_mimeType);
+    if(!m_description.isEmpty())
+        js["description"] = QString(m_description);
+    js["contentHints"] = (QJsonObject)m_contentHints;
+    if(!m_name.isEmpty())
+        js["name"] = QString(m_name);
+    if(!m_originalFilename.isEmpty())
+        js["originalFilename"] = QString(m_originalFilename);
+    js["parent"] = ingrl_list2jsonarray(m_parent);
+    js["starred"] = m_starred;
+}
+
+void CreateFileDetails::fromJson(const QJsonObject& js){
+
+    m_id = js["id"].toString();
+    m_mimeType = js["mimeType"].toString();
+    m_description = js["description"].toString();
+    m_contentHints.fromJson(js["contentHints"].toObject());
+    m_name = js["name"].toString();
+    m_originalFilename = js["originalFilename"].toString();
+    jsonarray2ingrl_list(js["parent"].toArray(), m_parent);
+    m_starred = js["starred"].toVariant().toBool();
+}
+
+QString CreateFileDetails::toString(bool multiline)const
+{
+    QJsonObject js;
+    toJson(js);
+    QJsonDocument doc(js);
+    QString s(doc.toJson(multiline ? QJsonDocument::Indented : QJsonDocument::Compact));
+    return s;
+}
+
+
+std::unique_ptr<CreateFileDetails>  CreateFileDetails::factory::create(const QByteArray& data)
+{
+    QJsonDocument doc = QJsonDocument::fromJson(data);
+    QJsonObject js = doc.object();
+    return create(js);
+}
+
+
+std::unique_ptr<CreateFileDetails>  CreateFileDetails::factory::create(const QJsonObject& js)
+{
+    std::unique_ptr<CreateFileDetails> rv;
+    rv = std::unique_ptr<CreateFileDetails>(new CreateFileDetails);
+    rv->fromJson(js);
+    return rv;
+}
+
+#ifdef API_QT_AUTOTEST
+std::unique_ptr<CreateFileDetails> CreateFileDetails::EXAMPLE(int context_index){
+    Q_UNUSED(context_index);
+    static int example_idx = 0;
+    example_idx++;
+    std::unique_ptr<CreateFileDetails> rv(new CreateFileDetails);
+    rv->m_id = ApiAutotest::INSTANCE().getId("files::CreateFileDetails", example_idx);
+    rv->m_mimeType = QString("mimeType_%1").arg(example_idx);
+    rv->m_description = QString("description_%1").arg(example_idx);
+    rv->m_contentHints = *(files::ContentHints::EXAMPLE(0).get());
+    rv->m_name = QString("name_%1").arg(example_idx);
+    rv->m_originalFilename = QString("originalFilename_%1").arg(example_idx);
+    std::list<QString> list_of_parent;
+    for(int i = 0; i < 3; i++){
+        rv->m_parent.push_back(QString("_%1_%2").arg(i).arg(example_idx));
+    }
+    return rv;
+}
+#endif //API_QT_AUTOTEST
+
+}//files
+}//googleQt
---
+++ src/gdrive/files/FilesCreateFileDetails.h
@@ -0,0 +1,177 @@
+/**********************************************************
+ DO NOT EDIT
+ This file was generated from stone specification "files"
+ www.prokarpaty.net
+***********************************************************/
+
+#pragma once
+#include "google/endpoint/ApiUtil.h"
+#include "gdrive/GdriveRequestArg.h"
+#include "gdrive/files/FilesContentHints.h"
+
+namespace googleQt{
+namespace files{
+    class CreateFileDetails{
+        /**
+            field: id: The ID of the file.
+            field: mime_type: The MIME type of the file. Drive will attempt to
+                automatically detect an appropriate value from uploaded content
+                if no value is provided. The value cannot be changed unless a
+                new revision is uploaded.
+            field: description: A short description of the file.
+            field: content_hints: Additional information about the content of
+                the file.
+            field: name: The name of the file. This is not necessarily unique
+                within a folder.
+            field: original_filename: The original filename of the uploaded
+                content if available, or else the original value of the name
+                field. This is only available for files with binary content in
+                Drive.
+            field: parent: The IDs of the parent folders which contain the file.
+                If not specified as part of a create request, the file will be
+                placed directly in the My Drive folder. Update requests must use
+                the addParents and removeParents parameters to modify the
+                values.
+            field: starred: Whether the user has starred the file.
+        */
+
+    public:
+        CreateFileDetails():
+        m_starred(false)
+        {};
+
+        CreateFileDetails(const QString& arg):
+        m_starred(false)
+        { m_id = arg; };
+
+    public:
+            /**
+                The ID of the file.
+            */
+        QString id()const{return m_id;};
+        CreateFileDetails& setId(const QString& arg){m_id=arg;return *this;};
+
+            /**
+                The MIME type of the file. Drive will attempt to automatically
+                detect an appropriate value from uploaded content if no value is
+                provided. The value cannot be changed unless a new revision is
+                uploaded.
+            */
+        QString mimetype()const{return m_mimeType;};
+        CreateFileDetails& setMimetype(const QString& arg){m_mimeType=arg;return *this;};
+
+            /**
+                A short description of the file.
+            */
+        QString description()const{return m_description;};
+        CreateFileDetails& setDescription(const QString& arg){m_description=arg;return *this;};
+
+            /**
+                Additional information about the content of the file.
+            */
+        const ContentHints& contenthints()const{return m_contentHints;};
+        CreateFileDetails& setContenthints(const ContentHints& arg){m_contentHints=arg;return *this;};
+
+            /**
+                The name of the file. This is not necessarily unique within a
+                folder.
+            */
+        QString name()const{return m_name;};
+        CreateFileDetails& setName(const QString& arg){m_name=arg;return *this;};
+
+            /**
+                The original filename of the uploaded content if available, or
+                else the original value of the name field. This is only
+                available for files with binary content in Drive.
+            */
+        QString originalfilename()const{return m_originalFilename;};
+        CreateFileDetails& setOriginalfilename(const QString& arg){m_originalFilename=arg;return *this;};
+
+            /**
+                The IDs of the parent folders which contain the file. If not
+                specified as part of a create request, the file will be placed
+                directly in the My Drive folder. Update requests must use the
+                addParents and removeParents parameters to modify the values.
+            */
+        const std::list <QString>& parent()const{return m_parent;};
+        CreateFileDetails& setParent(const std::list <QString>& arg){m_parent=arg;return *this;};
+
+            /**
+                Whether the user has starred the file.
+            */
+        bool starred()const{return m_starred;};
+        CreateFileDetails& setStarred(const bool& arg){m_starred=arg;return *this;};
+
+    public:
+        operator QJsonObject ()const;
+        virtual void fromJson(const QJsonObject& js);
+        virtual void toJson(QJsonObject& js)const;
+        virtual QString toString(bool multiline = true)const;
+
+
+        class factory{
+        public:
+            static std::unique_ptr<CreateFileDetails>  create(const QByteArray& data);
+            static std::unique_ptr<CreateFileDetails>  create(const QJsonObject& js);
+        };
+
+
+        #ifdef API_QT_AUTOTEST
+        static std::unique_ptr<CreateFileDetails> EXAMPLE(int context_index);
+        #endif //API_QT_AUTOTEST
+
+
+    protected:
+            /**
+                The ID of the file.
+            */
+        QString m_id;
+
+            /**
+                The MIME type of the file. Drive will attempt to automatically
+                detect an appropriate value from uploaded content if no value is
+                provided. The value cannot be changed unless a new revision is
+                uploaded.
+            */
+        QString m_mimeType;
+
+            /**
+                A short description of the file.
+            */
+        QString m_description;
+
+            /**
+                Additional information about the content of the file.
+            */
+        ContentHints m_contentHints;
+
+            /**
+                The name of the file. This is not necessarily unique within a
+                folder.
+            */
+        QString m_name;
+
+            /**
+                The original filename of the uploaded content if available, or
+                else the original value of the name field. This is only
+                available for files with binary content in Drive.
+            */
+        QString m_originalFilename;
+
+            /**
+                The IDs of the parent folders which contain the file. If not
+                specified as part of a create request, the file will be placed
+                directly in the My Drive folder. Update requests must use the
+                addParents and removeParents parameters to modify the values.
+            */
+        std::list <QString> m_parent;
+
+            /**
+                Whether the user has starred the file.
+            */
+        bool m_starred;
+
+    };//CreateFileDetails
+
+}//files
+}//googleQt
