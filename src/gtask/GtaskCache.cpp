814b09e7ad55793367a886ac71a1f08f84945da3
Yuriy <osoft4ardi@gmail.com>
2018-07-07 09:10:39 -0400

    prepared task cache classes

---
+++ src/gtask/GtaskCache.cpp
@@ -0,0 +1,109 @@
+#include "Endpoint.h"
+#include "GtaskCache.h"
+#include "GtaskRoutes.h"
+#include "gtask/GtaskRequestArg.h"
+
+using namespace googleQt;
+using namespace gtask_cache;
+using namespace gtask;
+
+/**
+    TaskListReceiver
+*/
+TaskListReceiver::TaskListReceiver(GtaskRoutes& r) :m_r(r)
+{
+};
+
+GoogleTask<tasklists::TaskListResource>* TaskListReceiver::routeRequest(QString tasklist_id)
+{
+    TaskListContainerIdArg arg(tasklist_id);
+    return m_r.getTasklists()->get_Async(arg);
+};
+
+/**
+    TaskReceiver
+*/
+TaskReceiver::TaskReceiver(GtaskRoutes& r) :m_r(r)
+{
+};
+
+GoogleTask<tasks::TaskCollectionRes>* TaskReceiver::routeRequest(QString tasklist_id)
+{
+    TaskListArg arg(tasklist_id);
+    return m_r.getTasks()->list_Async(arg);
+};
+
+
+TaskList::TaskList(tasklists::TaskListResource* r) 
+{
+    setFromResource(r);
+};
+
+void TaskList::setFromResource(tasklists::TaskListResource* r) 
+{
+    m_id = r->id();
+    m_etag = r->etag();
+    m_title = r->title();
+    m_updated = r->updated();
+};
+
+
+/**
+    TaskCache
+*/
+TaskCache::TaskCache(ApiEndpoint& e) :m_endpoint(e)
+{
+    
+};
+
+void TaskCache::updateTaskList(tasklists::TaskListResource* r)
+{
+    auto i = m_id2tlist.find(r->id());
+    if (i != m_id2tlist.end()) {
+        i->second->setFromResource(r);
+    }
+    else {
+        TaskList::ptr p(new TaskList(r));
+        m_id2tlist[r->id()] = std::move(p);
+    }
+};
+
+/**
+   GtaskCacheRoutes
+*/
+GtaskCacheRoutes::GtaskCacheRoutes(GtaskRoutes& gtask_routes):m_t_routes(gtask_routes)
+{
+    m_cache.reset(new TaskCache(*(m_t_routes.endpoint())));
+};
+
+GoogleVoidTask* GtaskCacheRoutes::loadTaskLists(const TIDLIST& idlist)
+{
+    GoogleVoidTask* rv = m_t_routes.endpoint()->produceVoidTask();
+
+    auto par_runner = getTaskList_Async(idlist);
+    connect(par_runner, &EndpointRunnable::finished, [=]()
+    {
+        RESULT_LIST<tasklists::TaskListResource>&& res = par_runner->detachResult();
+        for (auto& m : res)
+        {
+            m_cache->updateTaskList(m.get());
+        }
+        par_runner->disposeLater();
+        rv->completed_callback();
+    });
+
+    return rv;
+};
+
+ConcurrentValueRunner<QString, TaskListReceiver, tasklists::TaskListResource>* GtaskCacheRoutes::getTaskList_Async(const TIDLIST& idlist) 
+{
+    std::unique_ptr<TaskListReceiver> mr(new TaskListReceiver(m_t_routes));
+
+    ConcurrentValueRunner<QString,
+        TaskListReceiver,
+        tasklists::TaskListResource>* r = new ConcurrentValueRunner<QString,
+        TaskListReceiver,
+        tasklists::TaskListResource>(idlist, std::move(mr), *(m_t_routes.endpoint()));
+    r->run();
+    return r;
+};
---
+++ src/gtask/GtaskCache.h
@@ -0,0 +1,128 @@
+#pragma once
+
+#include "google/endpoint/ApiUtil.h"
+#include "google/endpoint/ApiClient.h"
+#include "google/endpoint/BatchRunner.h"
+
+#include "gtask/tasks/TasksRoutes.h"
+#include "gtask/tasklists/TasklistsRoutes.h"
+
+namespace googleQt
+{
+    class Endpoint;
+    class GtaskRoutes;
+
+    namespace gtask_cache {
+        class TaskCache;
+        class GtaskCacheRoutes;
+
+        class TaskInfo
+        {
+        public:
+            using ptr  = std::shared_ptr<TaskInfo>;
+            //            using cptr  = std::shared_ptr<const TaskInfo>;
+            using tarr = std::vector<ptr>;
+
+            QString etag()const { return m_etag; }
+            QString id()const { return m_id; }
+            QString title()const { return m_title; }
+            QString content()const { return m_content; }
+            const QDateTime& updated()const { return m_updated; }            
+
+            tarr&   items(){return m_items;}
+
+        protected:
+            QString             m_etag, m_id, m_title, m_content;
+            QDateTime           m_updated;
+            tarr                m_items;
+        };
+
+        class TaskList
+        {
+        public:
+            class RootTaskInfo: public TaskInfo
+            {
+            public:
+                using ptr  = std::shared_ptr<RootTaskInfo>;
+            };
+
+            using ptr  = std::shared_ptr<TaskList>;
+
+            QString etag()const { return m_etag; }
+            QString id()const { return m_id; }
+            QString title()const { return m_title; }
+            const QDateTime& updated()const { return m_updated; }
+
+        protected:
+            TaskList(tasklists::TaskListResource* r);
+            void setFromResource(tasklists::TaskListResource* r);
+
+        protected:
+            QString             m_etag, m_id, m_title;
+            QDateTime           m_updated;
+            RootTaskInfo::ptr   m_root_task;
+
+            friend class TaskCache;
+        };
+        
+        class TaskCache
+        {
+        public:
+            using ptr = std::shared_ptr<TaskCache>;
+            using ID2TLIST = std::map<QString, TaskList::ptr>;
+       
+            ID2TLIST& all_task_lists(){return m_id2tlist;}
+
+        protected:
+            TaskCache(ApiEndpoint& e);
+            void updateTaskList(tasklists::TaskListResource*);
+
+        protected:
+            ID2TLIST m_id2tlist;
+            ApiEndpoint& m_endpoint;
+            friend class GtaskCacheRoutes;
+        };
+
+        /**
+            we know how to query for one task list, we can be inside concurrrent ValueBatchRunner
+        */
+        class TaskListReceiver
+        {
+        public:
+            TaskListReceiver(GtaskRoutes& r);
+            GoogleTask<tasklists::TaskListResource>* routeRequest(QString tasklist_id);
+        protected:
+            GtaskRoutes&        m_r;            
+        };
+
+        class TaskReceiver
+        {
+        public:
+            TaskReceiver(GtaskRoutes& r);
+            GoogleTask<tasks::TaskCollectionRes>* routeRequest(QString tasklist_id);
+        protected:
+            GtaskRoutes&        m_r;
+        };
+
+
+        class GtaskCacheRoutes : public QObject
+        {
+            Q_OBJECT
+        public:
+            using TIDLIST = std::list<QString>;
+
+            GtaskCacheRoutes(GtaskRoutes& gtask_routes);
+
+            GoogleVoidTask* loadTaskLists(const TIDLIST& idlist);
+
+        protected:
+            ConcurrentValueRunner<QString, TaskListReceiver, tasklists::TaskListResource>* getTaskList_Async(const TIDLIST& idlist);
+
+        protected:
+            GtaskRoutes&    m_t_routes;
+            TaskCache::ptr  m_cache;
+        };
+        
+    };
+
+};
